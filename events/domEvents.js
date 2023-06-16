9a25ff0dcaaea934919f2ee777ccf53e7256c83e
Mason Austin <mason47austin@gmail.com>
2023-06-15 19:27:05 -0500

    created the ability to allow the user to delte orders

--- events/domEvents.js
+++ events/domEvents.js
@@ -2,6 +2,8 @@ import viewOrder from '../pages/orderDetails';
 import { getMenuItem } from '../api/menuData';
 import newOrderForm from '../components/newOrderForm';
 import addItemForm from '../pages/addItemForm';
+import { deleteOrder, getOrder } from '../api/orderData';
+import showOrderCards from '../pages/showOrder';
 
 const domEvents = () => {
   document.querySelector('#maincontainer').addEventListener('click', (e) => {
@@ -17,6 +19,12 @@ const domEvents = () => {
     if (e.target.id === 'add-menu-item') {
       console.warn('add menu item button clicked');
       addItemForm();
+    }
+    if (e.target.id.includes('delete-order')) {
+      const [, firebaseKey] = e.target.id.split('--');
+      deleteOrder(firebaseKey).then(() => {
+        getOrder().then(showOrderCards);
+      });
     }
   });
 };
