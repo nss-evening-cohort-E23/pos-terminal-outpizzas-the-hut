a324156d9e1c7542065cb3d45c581c7ef6d86984
Eli Bradford <elibradford227@gmail.com>
2023-06-17 12:32:29 -0500

    commit for pull

--- pages/home.js
+++ pages/home.js
@@ -5,7 +5,7 @@ const homePage = () => {
   clearDom();
   const domString = `
   <h1>Welcome user</h1>
-  <button type="button" class="btn btn-primary">View Orders</button>
+  <button type="button" class="btn btn-primary" id=>View Orders</button>
   <button id="create-order-btn" type="button" class="btn btn-primary">Create An Order</button>
   <button id="revenueBtn" type="button" class="btn btn-primary">View Revenue</button>
   `;
